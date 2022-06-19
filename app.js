649c6f5f69b47bbf1e0aef6e8fe322bfa6de012b
lucamaya <lukitasamaya21@gmail.com>
2022-06-19 15:34:55 -0300

    agrega rutas y vistas

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3030;
 
+//console.log(__dirname);
+
 app.use(express.static('public'));
 
-app.get('/',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')))
 
 
 
@@ -12,22 +15,15 @@ app.listen(port, () => console.log('server renning in http://localhost:' + port)
 
 
 
-app.get('/',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
 
-app.get('/babbage',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
-
-app.get('/barners-lee',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
-
-app.get('/clark',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
-
-app.get('/hamilton',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
-
-app.get('/hopper',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.htmll')))
-
-
-app.get('/lovelace',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
-
-app.get('/turing',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+app.get('/babbage',(req,res) => res.sendFile(path.join(__dirname, 'views', 'babbage.html')))
+app.get('/barners-lee',(req,res) => res.sendFile(path.join(__dirname, 'views', 'berners.html')))
+app.get('/clark',(req,res) => res.sendFile(path.join(__dirname, 'views', 'clark.html')))
+app.get('/hamilton',(req,res) => res.sendFile(path.join(__dirname, 'views', 'hamilton.html')))
+app.get('/hopper',(req,res) => res.sendFile(path.join(__dirname, 'views', 'hopper.htmll')))
+app.get('/lovelace',(req,res) => res.sendFile(path.join(__dirname, 'views', 'lovelace.html')))
+app.get('/turing',(req,res) => res.sendFile(path.join(__dirname, 'views', 'turing.html')))
 
 
 
