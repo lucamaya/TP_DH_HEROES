5648fb3ef890a6d705011da928c3f82c61bfbe3f
lucamaya <lukitasamaya21@gmail.com>
2022-06-19 14:13:03 -0300

    agregar estructura

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ const express = require('express');
 const app = express();
 const port = 3030;
 
+app.use(express.static('public'));
 
+app.get('/',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
 
 
 
 app.listen(port, () => console.log('server renning in http://localhost:' + port))
+
+
+
+app.get('/',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+
+app.get('/babbage',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+
+app.get('/barners-lee',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+
+app.get('/clark',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+
+app.get('/hamilton',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+
+app.get('/hopper',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.htmll')))
+
+
+app.get('/lovelace',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+
+app.get('/turing',(req.res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+
+
+
+
