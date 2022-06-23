dc0c0abbb247c24474a848da5ad66b6f61357b3c
lucamaya <lukitasamaya21@gmail.com>
2022-06-22 21:23:45 -0300

    terminado

--- app.js
+++ app.js
@@ -4,27 +4,25 @@ const app = express();
 const port = 3030;
 
 //console.log(__dirname);
-
+/* recusos estaticos */
 app.use(express.static('public'));
 
-app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')))
 
 
 
-app.listen(port, () => console.log('server renning in http://localhost:' + port))
 
 
-
+/* rutas */
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
 
 app.get('/babbage',(req,res) => res.sendFile(path.join(__dirname, 'views', 'babbage.html')))
-app.get('/barners-lee',(req,res) => res.sendFile(path.join(__dirname, 'views', 'berners.html')))
+app.get('/berners-lee',(req,res) => res.sendFile(path.join(__dirname, 'views', 'berners-lee.html')))
 app.get('/clark',(req,res) => res.sendFile(path.join(__dirname, 'views', 'clark.html')))
 app.get('/hamilton',(req,res) => res.sendFile(path.join(__dirname, 'views', 'hamilton.html')))
 app.get('/hopper',(req,res) => res.sendFile(path.join(__dirname, 'views', 'hopper.htmll')))
 app.get('/lovelace',(req,res) => res.sendFile(path.join(__dirname, 'views', 'lovelace.html')))
 app.get('/turing',(req,res) => res.sendFile(path.join(__dirname, 'views', 'turing.html')))
+ 
 
-
-
-
+/* servidor */
+app.listen(port, () => console.log('server renning in http://localhost:' + port))
